de09695f6869c8673a4c1b797d01e875f5c87e00
TJ-Projects3 <tjprojects3@gmail.com>
2024-11-08 18:16:57 -0500

    Rough draft of navbar

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,9 +1,22 @@
-import { AppBar, Toolbar } from "@mui/material";
+import { AppBar, Toolbar, Button, Typography, Image } from "@mui/material";
 
 export default function Navbar() {
     return (
-        <AppBar>
-            <Toolbar></Toolbar>
+        <AppBar className="nav-bar" position="fixed">
+            <Toolbar className="tool-bar">
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <img src="\tu-tiger.webp" width="50px"></img>
+                    <Typography className="logo-title" component="a" href="/" variant="h6" sx={{  textDecoration: 'none', color: 'black', paddingLeft: '10px'}}>
+                        Coursesight
+                    </Typography>
+                </div>
+                <div>
+                    <Button color="inherit" href="/pricing" className="nav-item">Pricing</Button>
+                    <Button color="inherit" href="/about" className="nav-item">About</Button>
+                    <Button color="inherit" href="/resources" className="nav-item">Resource</Button>
+                    <Button color="inherit" href="/contact" className="nav-item">Contact</Button>    
+                </div>
+            </Toolbar>
         </AppBar>
     )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
+'use client'
 import Image from "next/image";
 import { Box } from "@mui/material";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <Box width={'100vw'} height={'100vh'}>
-        
+          <Navbar></Navbar>
     </Box>
   );
 }
